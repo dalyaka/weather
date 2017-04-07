c37c9a0b89c358e67526cf1f7a4ae5f72da9e847
Idaliya <idaliya.taisina@gmail.com>
2017-04-07 16:16:59 +0300

    add crossorigin to api url

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,5 +1,5 @@
 export const settings = {
-  apiUrl: 'http://api.openweathermap.org',
+  apiUrl: process.env.NODE_ENV !== 'production' ? 'http://api.openweathermap.org' : ' https://crossorigin.me/http://api.openweathermap.org',
   baseUrl: process.env.NODE_ENV !== 'production' ? '/Users/dalyaka/my/weather/index.html' : '/weather/assets/',
   openWeatherApiParams: 'appid=c260fa56e60ab19059cabec7029ac520&lang=ru',
 };
