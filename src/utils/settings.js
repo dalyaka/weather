3387548a7dc6cc1b35a80dbe24dd1e2436ba71d2
Idaliya <idaliya.taisina@gmail.com>
2017-04-07 15:48:12 +0300

    prod

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,5 +1,5 @@
 export const settings = {
   apiUrl: 'http://api.openweathermap.org',
-  baseUrl: process.env.NODE_ENV === 'production' ? '/weather/' : '/Users/dalyaka/my/weather/index.html',
+  baseUrl: process.env.NODE_ENV === 'production' ? '/weather/assets/' : '/Users/dalyaka/my/weather/index.html',
   openWeatherApiParams: 'appid=c260fa56e60ab19059cabec7029ac520&lang=ru',
 };
